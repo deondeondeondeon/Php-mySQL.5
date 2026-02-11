e5ac837d405c198533000474f51bd0de85baf367
deondeondeondeon <qirezideon@gmail.com>
2026-02-11 20:49:37 +0100

    modul  1,2

---
+++ module2/variables.php
@@ -0,0 +1,13 @@
+<?php
+    $school ='Digital School';
+
+    echo "I love $school";
+
+    $name = 'John';
+    $age = 25;
+    $price = 19.99;
+    $is_logged_in = true;
+    $not_logged_in = false;
+    $colors = array("red","green","blue");
+    $var = NULL;
+?>
