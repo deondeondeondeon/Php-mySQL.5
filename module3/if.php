3382dc603599ee1fd179477060356d36031cac12
deondeondeondeon <qirezideon@gmail.com>
2026-02-18 20:48:58 +0100

    modul3

---
+++ module3/if.php
@@ -0,0 +1,28 @@
+<?php
+    //$num = -5;
+    //if ($num < 0){
+    //    echo "$num is less than 0";
+    //}
+
+    //$age = 15;
+    //if( $age < 18 ){
+    //    echo "you are under 18!";
+    //}else{
+    //    echo "You are an adult!";
+    //}
+
+    //$age = 15;
+    //if((age > 12) && ($age < 18)){
+    //    echo "You are a teenager";
+    //}
+
+    $num = 5;
+
+    if ($num < 0){
+        echo "The value of $num is a negative number.";
+    }elseif($num==0){
+        echo "The value of $num is 0. ";
+    }else{
+        echo "The value of $num is a positive number.";
+    }
+?>
