07803f7ec22c4bde6ebfad076cfa92d53429acee
deondeondeondeon <qirezideon@gmail.com>
2026-03-02 20:49:22 +0100

    modul4

---
+++ module4/build_in_function.php
@@ -0,0 +1,17 @@
+<?php 
+    //build in 
+   // phpinfo();
+
+    //$x = "Holle";
+    //prnt_r($x);
+
+    //$x = ;
+    //$y = 10.3;
+    //$z = "hello";
+
+    //echo gettype($x)."<br>"
+    echo gettype($y)."<br>";
+    echo gettype($z)."<br";
+
+    echo phpversion();
+    ?>
--- module4/user_defined_functions.php
+++ module4/user_defined_functions.php
@@ -11,31 +11,46 @@
     //}
     //echo sum(10, 30);
 
-    function maximum($x, $y){
-        if($x > $y){
-            return $x;
-        }else{
-            return $y;
-        }
-    }
+    //function maximum($x, $y){
+    //  if($x > $y){
+    //       return $x;
+    //    }else{
+    //        return $y;
+    //    }
+    //}
 
-    $result = maximum(10, 15);
-    echo "The max is: ".$result;
+    //$result = maximum(10, 15);
+    //echo "The max is: ".$result;
 
     // Function to check a random number assigned is fully divisible by the number 2
 
-    function isDivisibleByTwo($number){
-        if($number % 2 == 0){
-            return true;
-        }else{
-            return false;
-        }
-    }
+    //function isDivisibleByTwo($number){
+    //    if($number % 2 == 0){
+    //        return true;
+    //    }else{
+    //        return false;
+    //    }
+    //}
 
-    $randomNumber = 12;
-    if (isDivisibleByTwo($randomNumber)){
-        echo "The number $randomNumber is divisible by 2.";
-    }else{
-        echo"The number $randomNumber is not divisible by 2.";
+    //$randomNumber = 12;
+    //if (isDivisibleByTwo($randomNumber)){
+    //    echo "The number $randomNumber is divisible by 2.";
+    //}else{
+    //    echo"The number $randomNumber is not divisible by 2.";
+    //}
+
+    //function maximum($x, $y){
+    //    if($x > $y){
+    //        return $x;
+    //   }else{
+    //        return $y;
+    //    }
+    //}
+
+    //result = maximum(10, 15);
+    //echo "The max is :".$result;
+
+    function fully_divisible($n){
+        
     }
 ?>
